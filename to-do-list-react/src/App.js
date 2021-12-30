a6d071369301010f5585c9d6711d4fcded7d6652
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-12-30 20:31:29 -0300

    add function APP  para adicionar tarefa

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
 
 function App() {
+  const handleChange = (event) =>{
+    const inpuTask = event.target.value
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>ToDo List</h1>
+      <form >
+        <input type="text" placeholder="Adicione uma tarefa" onChange = {handleChange}/>
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <ul className="todo-list" >
+        <li>Item</li>
+      </ul>
     </div>
   );
 }
