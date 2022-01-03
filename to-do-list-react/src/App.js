18d0797510e44dc2b825f459fe22d8254873d39b
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2022-01-03 18:09:35 -0300

    mapeando os items da lista

--- to-do-list-react/src/App.js
+++ to-do-list-react/src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 function App() {
-  const handleChange = (event) =>{
+
+  const [task, setTask] = useState('');
+  const [itemsList, setItemsList ] = useState([]);
+  
+  function handleChange(event){
     const inpuTask = event.target.value
+    setTask(inpuTask);
   }
+
+  function addItemToList(){
+    console.log(task)
+    setItemsList([...itemsList, task]);
+    setTask('')
+  }
+
   
   return (
     <div className="App">
        <h1>ToDo List</h1>
       <form >
-        <input type="text" placeholder="Adicione uma tarefa" onChange = {handleChange}/>
-        <button type="submit">Adicionar</button>
+        <input type="text" placeholder="Adicione uma tarefa" onChange = { handleChange } value={ task }/>
+        <button type="button" onClick={ addItemToList }>Adicionar</button>
       </form>
 
-      <ul className="todo-list" >
-        <li>Item</li>
-      </ul>
+      {itemsList.map((item, index) => (<div key={index}> {item} </div>) )}
+
     </div>
   );
 }
